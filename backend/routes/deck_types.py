304855e59c3f846bb604bfc3cc5c60c3fcf61975
Gastón Aliaga <galiaga@gmail.com>
2025-03-12 13:50:31 -0300

    TCG-43 Captures deck_type in frontend using SQLAlchemy

---
+++ backend/routes/deck_types.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required
+from backend import db
+from backend.models.deck_type import DeckType
+
+deck_types_bp = Blueprint('deck_types', __name__)
+
+@deck_types_bp.route('/deck_types', methods=['GET'])
+@jwt_required()
+def get_deck_types():
+    deck_types = DeckType.query.all()
+    return jsonify([{'id': dt.id, 'deck_type': dt.deck_type} for dt in deck_types])
