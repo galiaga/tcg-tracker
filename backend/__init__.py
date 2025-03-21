bb15adb1ab938bdd92d4f211267d6fa3771c8183
Gastón Aliaga <galiaga@gmail.com>
2025-03-21 16:38:39 -0300

    TCG-58: Add GitHub Actions workflow for running tests

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,12 +5,21 @@ import os
 
 db = SQLAlchemy(session_options={"autocommit": False, "autoflush": False})
 
-def create_app():
+def create_app(config_name=None):
     app = Flask(__name__)
 
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    if config_name == "testing":
+        app.config.update(
+            TESTING=True,
+            SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            WTF_CSRF_ENABLED=False,
+            JWT_SECRET_KEY="testing-secret"
+        )
+    else:
+        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
